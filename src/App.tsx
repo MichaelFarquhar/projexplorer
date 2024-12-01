93bfc760e07542d96f6f79d17127fb07e24168c5
Michael Farquhar <michaelfarquhar4@gmail.com>
2024-12-01 18:40:07 -0500

    Add top toolbar with placeholder buttons and breadcrumbs

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import { Button } from "react-aria-components";
+import { TopToolbar } from "./layouts/Toolbar/TopToolbar";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -14,7 +15,8 @@ function App() {
   }
 
   return (
-    <main className="container">
+    <main>
+      <TopToolbar />
       <h1>Welcome to Tauri + React</h1>
 
       <div className="row">
---
+++ src/layouts/Toolbar/TopToolbarIconButton.tsx
@@ -0,0 +1,23 @@
+import { Button, Tooltip, TooltipTrigger } from "react-aria-components";
+
+interface Props {
+  label: string;
+  children: React.ReactNode;
+  onPress?: () => void;
+}
+
+export const TopToolbarIconButton = ({ label, children }: Props) => {
+  return (
+    <TooltipTrigger delay={0} closeDelay={0}>
+      <Button className="p-2.5 bg-transparent border-none shadow-none transition hover:bg-slate-300 dark:hover:bg-slate-700 rounded-md cursor-pointer">
+        {children}
+      </Button>
+      <Tooltip
+        placement="bottom"
+        className="mt-1 px-2.5 py-1.5 text-xs font-medium text-white bg-slate-300 rounded-lg shadow-sm tooltip dark:bg-slate-700"
+      >
+        {label}
+      </Tooltip>
+    </TooltipTrigger>
+  );
+};
